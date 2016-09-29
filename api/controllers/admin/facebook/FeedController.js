7e0afb194740f5a98f6362f4b946ab798fc69d40
smlsunxie <smlsun@gmail.com>
2016-09-29 11:58:16 +0800

    fix facebook feed index can not show

--- api/controllers/admin/facebook/FeedController.js
+++ api/controllers/admin/facebook/FeedController.js
@@ -2,25 +2,26 @@ module.exports = {
   index: async (req, res) => {
     res.ok({
       view: true,
-      layout: '../../admin/default/index'
+      layout: 'admin/default/index'
     });
   },
   create: async (req, res) => {
     res.ok({
       view: true,
-      layout: '../admin/default/index'
+      layout: 'admin/default/create'
     });
   },
   edit: async (req, res) => {
     res.ok({
       view: true,
-      layout: '../admin/default/index'
+      layout: 'admin/default/edit'
     });
+
   },
   show: async (req, res) => {
     res.ok({
       view: true,
-      layout: '../admin/default/index'
+      layout: 'admin/default/show'
     });
   },
-};
+}
