053e1f9f33781359f81f01ccf90ceb27e9307136
FuYaoDe <dan82625@gmail.com>
2016-09-30 18:10:13 +0800

    add contact front end

--- api/controllers/ContactController.js
+++ api/controllers/ContactController.js
@@ -2,7 +2,12 @@ module.exports = {
   index: async (req, res) => {
     try {
       const data = {};
-      res.view('contact/index', {data});
+      res.view('contact/index', {
+        data: {
+          user: AuthService.getSessionUser(req) || {},
+          reCAPTCHAKey: sails.config.reCAPTCHA.key,
+        }
+      });
     } catch (e) {
       res.serverError(e);
     }
