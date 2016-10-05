d850e850c526e661f37d383c35f896afd51c3c78
smlsunxie <smlsun@gmail.com>
2016-10-05 14:29:07 +0800

    add ImageUpload spec

---
+++ test/unit/controllers/api/admin/ImageController.spec.js
@@ -0,0 +1,17 @@
+
+describe.only("ImageController test", function() {
+  it("create image by file attachment", async function(done) {
+
+    try {
+      const res = await request(sails.hooks.http.app)
+      .post("/api/admin/upload")
+      .attach('uploadPic', __dirname+'/test.png')
+
+      const {body} = res;
+
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+});
