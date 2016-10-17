9ac11760aabd51615026324402474200efa639f9
KenYA <tikka0910@gmail.com>
2016-10-17 11:51:34 +0800

    啟動伺服器時，檢查是否有 facebook Page or App ID

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -115,6 +115,10 @@ module.exports.bootstrap = async (cb) => {
     if (sails.config.google===undefined || sails.config.google.key===undefined) {
       throw('google api Key not exist!!');
     }
+    //檢查 FB Page & App ID 是否存在
+    if (sails.config.facebook === undefined || sails.config.facebook.pageId === '' || sails.config.facebook.appId === ''){
+      sails.log.error('Facebook Page ID or App ID not exist!!');
+    }
 
     cb();
   } catch (e) {
