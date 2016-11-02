498d49f5d7bb1fc2e61537859589a0b5fe73333b
KenYA <tikka0910@gmail.com>
2016-11-02 10:03:01 +0800

    修改 exportExcel 輸出資料

--- api/services/ExportService.js
+++ api/services/ExportService.js
@@ -52,44 +52,19 @@ module.exports = {
 
   exportExcel: async ({format, content, fileName, columns}) => {
     try {
-      // if (format) {
-      //   content = format(content);
-      // }
-      // sails.log.debug(content);
-      // let dataString = await new Promise((defer, reject) => {
-      //   stringify(content, { header: !!columns, columns }, function(err, output){
-      //     if (err) reject(err);
-      //     defer(output);
-      //   });
-      // });
-      // sails.log.debug(dataString);
-      // dataString = "\uFEFF" + dataString
-      // const encoding = 'utf-8';
-      // let dataBuffer = new Buffer(dataString);
-      // dataBuffer = iconv.encode(dataBuffer, encoding);
-      // const time = moment(new Date()).format("YYYYMMDDHHmmSS");
-      // fileName = `${fileName || ''}${time}.csv`;
-      // const filePath = `${__dirname}/../../.tmp/${fileName}`;
-      // await fs.writeFileSync(filePath, dataBuffer);
+      if (format) {
+        content = format(content);
+      }
+      let conf = {};
+      conf.name = fileName;
+      conf.cols = columns;
+      conf.rows = content;
 
-      var conf ={};
       // conf.stylesXmlFile = `${__dirname}/styles.xml`;
-      conf.name = "mysheet";
-      conf.cols = [{
-        caption: '測試',
-        type: 'string',
-      }];
-      conf.rows = [
-        ["周咏蒨"],
-        ["등원청"],
-        ["訂購者"],
-        ["안갯길"],
-        ["羅筑儀"],
-        ["香港尖沙咀金马伦道48号中国保险大厦15楼B室"],
-        ["20life LM•BU•WH"],
-      ];
       var result = nodeExcel.execute(conf);
-      // let dataBuffer = new Buffer(result);
+      // console.log("nodeExcel==>",result);
+      let dataBuffer = new Buffer(result, 'binary');
+      // console.log("dataBuffer==>",dataBuffer);
       const time = moment(new Date()).format("YYYYMMDDHHmmSS");
       fileName = `${fileName || ''}${time}.xlsx`;
       const filePath = `${__dirname}/../../.tmp/${fileName}`;
