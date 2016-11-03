4c708004ae827a6264bb798edc742ec272e91b85
KenYA <tikka0910@gmail.com>
2016-11-03 15:53:12 +0800

    eventallpay controller init

---
+++ api/controllers/api/admin/EventAllpayController.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+  find: async (req, res) => {
+    try {
+      // const { query } = req;
+      const { serverSidePaging } = req.query;
+      const query = req.body;
+      const modelName = req.options.controller.split("/").reverse()[0];
+      let result;
+      if (serverSidePaging) {
+        const include = {
+          model: EventOrder,
+          include: [User, Event]
+        }
+        result = await PagingService.process({ query, modelName, include });
+      } else {
+        const items = await sails.models[modelName].findAll({
+          include:{
+            model: EventOrder,
+            include: [User, Event]
+          }
+        });
+        result = { data: { items } };
+      }
+      res.ok(result);
+    } catch (e) {
+      res.serverError(e);
+    }
+  },
+
+  findOne: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await Allpay.findOne({
+        where:{
+          id
+        },
+        include:{
+          model: EventOrder,
+          include: [User, Event]
+        }
+       });
+      res.ok({ data: { item } });
+    } catch (e) {
+      res.serverError(e);
+    }
+  },
+
+}
