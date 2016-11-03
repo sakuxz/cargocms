f5e80aea455ee4f200e408bfed8cc8944b6d8777
sakuxz <yui12327@gmail.com>
2016-11-03 12:05:22 +0800

    add getTicketStatus in EventService

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -30,6 +30,12 @@ module.exports = {
         return res.redirect(data.url)
       }
       const social = SocialService.forPost({posts: [data]});
+
+      data.Events.forEach((e) => {
+        e = Object.assign(e, EventService.getTicketStatus(e, new Date()));
+        console.log(e);
+      });
+
       res.view('event/show', {data, social});
     } catch (e) {
       res.serverError(e);
---
+++ api/services/EventService.js
@@ -0,0 +1,20 @@
+import FacebookHelper from './libraries/facebook/';
+import moment from 'moment';
+
+module.exports = {
+  getTicketStatus: ({ sellStartDate, sellEndDate }, now) => {
+    let open = false;
+    let status = '';
+    if (moment(now).isBefore(sellStartDate)){
+      open = false;
+      status = '尚未開賣';
+    } else if(moment(now).isAfter(sellEndDate)){
+      open = false;
+      status = '結束販售';
+    } else {
+      open = true;
+      status = '訂購'
+    }
+    return { open, status }
+  }
+}
