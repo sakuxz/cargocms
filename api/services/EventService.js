08b81d838b0679c67d539bccd1d596152aa4417a
FuYaoDe <dan82625@gmail.com>
2016-11-08 11:06:04 +0800

    新增內容選擇類別到最上面，修正排序

--- api/services/EventService.js
+++ api/services/EventService.js
@@ -1,13 +1,13 @@
 import moment from 'moment';
 
 module.exports = {
-  getTicketStatus: ({ sellStartDate, sellEndDate }, now) => {
+  getTicketStatus: ({ sellStartDate, sellEndDate, limit, signupCount }, now) => {
     let open = false;
     let status = '';
     if (moment(now).isBefore(sellStartDate)){
       open = false;
       status = '尚未開賣';
-    } else if(moment(now).isAfter(sellEndDate)){
+    } else if(moment(now).isAfter(sellEndDate) || limit <= signupCount){
       open = false;
       status = '結束販售';
     } else {
