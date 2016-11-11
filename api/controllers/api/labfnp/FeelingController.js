adce79d0a0df99ad76a93e4a4e38569c59e2818f
KenYA <tikka0910@gmail.com>
2016-11-11 18:06:47 +0800

    feeling controller , create 修改 create 方式，考慮相同 title 需增加 totalRepeat

--- api/controllers/api/labfnp/FeelingController.js
+++ api/controllers/api/labfnp/FeelingController.js
@@ -64,7 +64,34 @@ module.exports = {
   create: async (req, res) => {
     try {
       const data = req.body;
-      const item = await Feeling.create(data);
+      let item = {};
+
+      let sameTitle = await Feeling.findOne({
+        where:{
+          title: data.title
+        }
+      });
+
+      data.totalRepeat = "1";
+      data.score = "1";
+
+      if (!sameTitle){
+        item = await Feeling.create(data);
+      } else {
+        let newTotalRepeat = ( Number(sameTitle.totalRepeat) + 1).toString();
+
+        sameTitle = await Feeling.update({
+          totalRepeat: newTotalRepeat
+        },{
+          where:{
+            title: data.title
+          }
+        });
+
+        data.totalRepeat = newTotalRepeat;
+        item = await Feeling.create(data);
+      }
+
       let message = 'Create success.';
       res.ok({ message, data: { item } } );
     } catch (e) {
