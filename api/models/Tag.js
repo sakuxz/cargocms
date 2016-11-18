4c1b7ab8d53276834bdaaa36ab8f868f8879d07b
KenYA <tikka0910@gmail.com>
2016-11-18 11:17:51 +0800

    將其他model 都補上 dateTime 虛擬欄位

--- api/models/Tag.js
+++ api/models/Tag.js
@@ -3,6 +3,27 @@ module.exports = {
     title: {
       type: Sequelize.STRING,
     },
+    createdDateTime:{
+      type: Sequelize.VIRTUAL,
+      get: function(){
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('createdAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    },
+
+    updatedDateTime:{
+      type: Sequelize.VIRTUAL,
+      get: function(){
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('updatedAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    }
   },
   associations: () => {
     Tag.belongsToMany(Post,  {
