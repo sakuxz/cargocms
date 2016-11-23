973d0a6512f5651e66392048b210fd9982ccbd8a
KenYA <tikka0910@gmail.com>
2016-11-23 09:54:18 +0800

    修正 資料庫 migration table Name

--- migration/v6/migrate.sql
+++ migration/v6/migrate.sql
@@ -1,4 +1,4 @@
-ALTER TABLE RecipeOrder MODIFY COLUMN productionStatus enum("NEW","PENDING", "RECEIVED", "REQUESTED", "SUBMITTED", "PAID", "PROCESSING", "CANCELLED", "SHIPPED", "DELIVERED", "COMPLETED");
-ALTER TABLE EventOrder MODIFY COLUMN productionStatus enum("NEW","PENDING", "RECEIVED", "REQUESTED", "SUBMITTED", "PAID", "PROCESSING", "CANCELLED", "SHIPPED", "DELIVERED", "COMPLETED");
+ALTER TABLE RecipeOrders MODIFY COLUMN productionStatus enum("NEW","PENDING", "RECEIVED", "REQUESTED", "SUBMITTED", "PAID", "PROCESSING", "CANCELLED", "SHIPPED", "DELIVERED", "COMPLETED");
+ALTER TABLE EventOrders MODIFY COLUMN productionStatus enum("NEW","PENDING", "RECEIVED", "REQUESTED", "SUBMITTED", "PAID", "PROCESSING", "CANCELLED", "SHIPPED", "DELIVERED", "COMPLETED");
 
-UPDATE RecipeOrder JOIN Allpay ON Allpay.`RecipeOrderId` = RecipeOrder.`id` SET RecipeOrder.`productionStatus` = "PENDING" WHERE Allpay.PaymentType = 'aio';
+UPDATE RecipeOrders JOIN Allpays ON Allpays.`RecipeOrderId` = RecipeOrders.`id` SET RecipeOrders.`productionStatus` = "PENDING" WHERE Allpays.PaymentType = 'aio';
