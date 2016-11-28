a19e5561c132c025061ce0da90d46ba558f0208c
smlsunxie <smlsun@gmail.com>
2016-11-28 21:09:11 +0800

    add memory use after before

--- test/stress/Traffic.spec.js
+++ test/stress/Traffic.spec.js
@@ -3,11 +3,12 @@ describe("traffic test", function() {
   const spawn = require('child_process').spawn;
 
   it("make an simple traffic test", async function(done) {
-    
+
     try {
       const url = "http://"+sails.config.appUrl+"/";
       //const ls = spawn('curl', [url]);
       //console.log(url);
+      var beforeTestMemoryUsage = process.memoryUsage();
       const command = spawn('artillery', ['quick','--duration','60','--rate','10','-n','20',url]);
 
       command.stdout.on('data', (data) => {
@@ -19,6 +20,12 @@ describe("traffic test", function() {
       });
 
       command.on('close', (code) => {
+        var afterTestMemoryUsage = process.memoryUsage();
+        console.log("=== before traffic test memoryUsage ===");
+        console.log(beforeTestMemoryUsage);
+        console.log("=== after traffic test memoryUsage ===");
+        console.log(afterTestMemoryUsage);
+
         console.log(`child process exited with code ${code}`);
         if (code>=0) {
           done();
