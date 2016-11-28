35c3e02cf945c5d1450d34def72a2cd2eb651ccf
smlsunxie <smlsun@gmail.com>
2016-11-28 10:53:24 +0800

    remove log

--- newrelic.js
+++ newrelic.js
@@ -6,5 +6,4 @@ var config = require("./config/local.js")
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-console.log("=== config.newrelic ===", config.newrelic);
 exports.config = config.newrelic;
