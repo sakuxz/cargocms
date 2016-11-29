b1801b7fdd6ec4c5fc51dd72d850e07a9d976e9e
dd-han <han@dd-han.tw>
2016-11-29 14:12:11 +0800

    fix ident and package.json

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -4,31 +4,31 @@ var rc = require("rc");
 global.request = require("supertest-as-promised");
 global.should = require("chai").should();
 global.sinon = require("sinon");
-  before(function(done) {
-    var config = rc('sails');
-    if (process.env.TEST_MODE == "STRESS")  {
-      // ref from https://segmentfault.com/a/1190000004888348
-      var fs = require('fs');
-      // (see note below)
-      setInterval(function takeSnapshot() {
-        var mem = process.memoryUsage();
-        fs.appendFile('./test/stress/output_serverMemory.xls', 
-          mem.rss / 1024 / 1024 + '\t' +
-          mem.heapUsed / 1024 / 1024 + '\t' + 
-          mem.heapTotal / 1024 / 1024 + '\n', 'utf8');
-      }, 1000); // Snapshot every second
-      config.environment = 'production';
-    }
-    else {
-      config.environment = 'test';
-    }
+before(function(done) {
+  var config = rc('sails');
+  if (process.env.TEST_MODE == "STRESS")  {
+    // ref from https://segmentfault.com/a/1190000004888348
+    var fs = require('fs');
+    // (see note below)
+    setInterval(function takeSnapshot() {
+      var mem = process.memoryUsage();
+      fs.appendFile('./test/stress/output_serverMemory.xls', 
+        mem.rss / 1024 / 1024 + '\t' +
+        mem.heapUsed / 1024 / 1024 + '\t' + 
+        mem.heapTotal / 1024 / 1024 + '\n', 'utf8');
+    }, 1000); // Snapshot every second
+    config.environment = 'production';
+  }
+  else {
+    config.environment = 'test';
+  }
 
-    Sails.lift(config, function(err, server) {
-      sails = server;
-      if (err) return done(err);
-      done(err, server);
-    });
+  Sails.lift(config, function(err, server) {
+    sails = server;
+    if (err) return done(err);
+    done(err, server);
   });
+});
 
 after(function(done) {
   sails.lower(done());
