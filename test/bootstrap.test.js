9c59d817e647d94f1ae2702df0f7000f34024c7e
dd-han <han@dd-han.tw>
2016-11-29 12:11:24 +0800

    better way to switch config in test

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -4,17 +4,34 @@ var rc = require("rc");
 global.request = require("supertest-as-promised");
 global.should = require("chai").should();
 global.sinon = require("sinon");
+if (process.env.TEST_MODE == "STRESS") 
 
-before(function(done) {
-  var config = rc('sails');
-  config.environment = 'stress';
 
-  Sails.lift(config, function(err, server) {
-    sails = server;
-    if (err) return done(err);
-    done(err, server);
+  before(function(done) {
+    var config = rc('sails');
+    if (process.env.TEST_MODE == "STRESS")  {
+      // ref from https://segmentfault.com/a/1190000004888348
+      var fs = require('fs');
+      // (see note below)
+      setInterval(function takeSnapshot() {
+        var mem = process.memoryUsage();
+        fs.appendFile('./test/stress/output_serverMemory.xls', 
+          mem.rss / 1024 / 1024 + '\t' +
+          mem.heapUsed / 1024 / 1024 + '\t' + 
+          mem.heapTotal / 1024 / 1024 + '\n', 'utf8');
+      }, 1000); // Snapshot every second
+      config.environment = 'production';
+    }
+    else {
+      config.environment = 'test';
+    }
+
+    Sails.lift(config, function(err, server) {
+      sails = server;
+      if (err) return done(err);
+      done(err, server);
+    });
   });
-});
 
 after(function(done) {
   sails.lower(done());
