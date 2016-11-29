155378727e83de90241101202d2d9006fa92e56e
dd-han <han@dd-han.tw>
2016-11-29 11:58:35 +0800

    finish basically procuction stress test for #1117

--- test/bootstrap.stress.js
+++ test/bootstrap.stress.js
@@ -1,6 +1,17 @@
 var Sails = require('sails');
 var rc = require("rc");
 
+// ref from https://segmentfault.com/a/1190000004888348
+var fs = require('fs');
+// (see note below)
+setInterval(function takeSnapshot() {
+  var mem = process.memoryUsage();
+  fs.appendFile('./test/stress/output_serverMemory.xls', 
+    mem.rss / 1024 / 1024 + '\t' +
+    mem.heapUsed / 1024 / 1024 + '\t' + 
+    mem.heapTotal / 1024 / 1024 + '\n', 'utf8');
+}, 1000); // Snapshot every second
+
 global.request = require("supertest-as-promised");
 global.should = require("chai").should();
 global.sinon = require("sinon");
--- test/stress/Traffic.spec.js
+++ test/stress/Traffic.spec.js
@@ -1,15 +1,19 @@
 describe("traffic test", function() {
   this.timeout(10000000);
+  const logPath = "test/stress/output_client/";
   const spawn = require('child_process').spawn;
+  let baseURL;
+  before(function(done) {
+    baseURL = "http://"+sails.config.appUrl+"/";
+    done();
+  });
 
   it("make an simple traffic test on index", async function(done) {
-
     try {
-      var beforeTestMemoryUsage = process.memoryUsage();
-
-      const baseURL = "http://"+sails.config.appUrl+"/";
-      const url = baseURL;
-      const command = spawn('artillery', ['quick','--duration','60','--rate','10','-n','20',url]);
+      let url = baseURL;
+      let logFilename = logPath + "index.json";
+      let beforeTestMemoryUsage = process.memoryUsage();
+      const command = spawn('artillery', ['quick','--duration','60','--rate','10','-n','20',url,'-o',logFilename]);
 
       command.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
@@ -20,7 +24,7 @@ describe("traffic test", function() {
       });
 
       command.on('close', (code) => {
-        var afterTestMemoryUsage = process.memoryUsage();
+        let afterTestMemoryUsage = process.memoryUsage();
         console.log("=== before traffic test memoryUsage ===");
         console.log(beforeTestMemoryUsage);
         console.log("=== after traffic test memoryUsage ===");
