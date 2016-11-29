f928961df0afb6120e24002f5a057e42df68bc4a
KenYA <tikka0910@gmail.com>
2016-11-29 14:26:37 +0800

    sysInfo 回傳資訊增加 diskUsage , session , Resident Set 記憶體使用資訊

--- api/controllers/UtilsController.js
+++ api/controllers/UtilsController.js
@@ -2,10 +2,16 @@
  * Utils Controller
  */
 const os = require('os-utils');
+const disk = require('diskusage');
 
 module.exports = {
-
   sysInfo: function(req, res) {
+
+    let diskUsage = 0;
+    disk.check('/', function(err, info) {
+      diskUsage = ( Number(info.available) / Number(info.total) ).toFixed(4);
+    });
+
     res.ok({
       sysUptime:          os.sysUptime(),
       processUptime:      os.processUptime(),
@@ -15,6 +21,9 @@ module.exports = {
       loadavg:            [os.loadavg(1), os.loadavg(5), os.loadavg(15)],
       cpuCount:           os.cpuCount(),
       platform:           os.platform(),
+      residentSet:        process.memoryUsage(),
+      diskUsage:          diskUsage,
+      session:            req.session,
     });
   },
 
