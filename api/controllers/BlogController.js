4e574242e4116c156f2af1a857a036a68016ce62
hsiangtzu <a841225@gmail.com>
2017-08-21 15:43:32 +0800

    修復lfp網誌頁(首頁+內頁)

--- api/controllers/BlogController.js
+++ api/controllers/BlogController.js
@@ -22,20 +22,20 @@ module.exports = {
 
   show: async (req, res) => {
     try {
-      const {id, name} = req.params
-      let data = await Post.findOne({
+      const { id, name } = req.params;
+      const data = await Post.findOne({
         where: { $or: [{ id: id || name }, { alias: name }] },
-        include: [ Tag, Image, User, Location]
+        include: [Tag, Image, User, Location],
       });
 
-      if(!data || !data.publish){
+      if (!data || !data.publish) {
         sails.log.error(`Post ID or Name: ${id || name}, data not found or not publish.`);
         return res.notFound();
       }
-      const social = SocialService.forPost({posts: [data]});
-      res.view('blog/show', {data, social});
+      const social = SocialService.forPost([data]);
+      res.view('blog/show', { data, social });
     } catch (e) {
       res.serverError(e);
     }
   },
-}
+};
